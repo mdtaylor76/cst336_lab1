2ebd34bcf83c28aac65e646cf2f53570eb4be89a
EC2 Default User <ec2-user@ip-172-31-14-73.us-west-1.compute.internal>
2020-10-24 23:28:25 +0000

    photo added

--- index.php
+++ index.php
@@ -1,11 +1,41 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> </title>
+        <title>Lab 1: Personal page </title>
+        <meta charset="utf-8">
     </head>
     <body>
-
-        <h1>Welcome to my website!</h1>
+        
+        <header>
+            <h1>Matt Taylor</h1>
+        </header>
+        
+        <hr>
+        
+        <nav>
+            
+            <a href="index.html"> Home </a>
+            <a href="about.html"> About </a>
+            <a href="contact.html"> Contact Me </a>
+            
+        </nav>
+        
+        <main>
+            
+            <figure>
+                
+                <img src="img/juan_doe.png"></img>
+                
+            </figure>
+            
+            <div>
+                
+                Welcome to my site!
+                
+            </div>
+            
+            
+        </main>
 
     </body>
 </html>
